5543dba6f432e3a412c85181794781e9609c384c
mester-ces <52388120+mester-ces@users.noreply.github.com>
2020-07-11 12:53:03 +0200

    Update despues-de-instalar-Arch-instalar-aplicaciones.sh

--- despues-de-instalar-Arch-instalar-aplicaciones.sh
+++ despues-de-instalar-Arch-instalar-aplicaciones.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo pacman -S xorg qtile gcc python-pip lightdm picom feh firfox firefox-i18n-es-es ligthdm-gtk-greeter vlc alsa-utils pulseaudio network-manager-applet volumeicon cbatticon upower pamixer 
-
+alacritty 
 pip install psutil
